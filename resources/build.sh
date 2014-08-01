09f420b074376e2f25348c77c38162ab419505b1
Lee Byron <lee@leebyron.com>
2014-08-01 00:17:44 -0700

    .

--- resources/build.sh
+++ resources/build.sh
@@ -3,5 +3,5 @@ lein do clean, cljsbuild once
 sed -e '/CLOSURE_OUTPUT/ {
   r dist/testcheck.js
   d
-}' resources/module.js > dist/testcheck.js.tmp
+}' resources/universal-module.js > dist/testcheck.js.tmp
 mv dist/testcheck.js.tmp dist/testcheck.js
