ac6a804b8a2c8fa6813a857e2803b85721e7036c
Lee Byron <lee@leebyron.com>
2016-12-16 11:15:55 -0800

    Remove stub dependency

--- src/cemerick/cljs/test.cljs
+++
@@ -1,7 +0,0 @@
-;; This is a stub dependency, since we're not using it and want to exclude
-;; it's exports.
-
-(ns cemerick.cljs.test)
-(def ^:dynamic *test-print-fn* nil)
-(def ^:dynamic *test-ctx* nil)
-(defmulti report :type)
