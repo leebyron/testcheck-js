ba398c369079d75d32bcf7e4354b4edc37a38e17
Lee Byron <lee@leebyron.com>
2016-12-20 13:51:40 -0800

    Nicer details

--- integrations/test-all.sh
+++ integrations/test-all.sh
@@ -2,19 +2,18 @@
 
 pushd ${BASH_SOURCE%/*} > /dev/null
 
-# Link testcheck
-npm link
+# Linking testcheck
+npm link > /dev/null
 
 # Loop through integrations, testing each
 for integration in ./*; do
   if [ -d "${integration}" ] ; then
-    pushd ${integration}
-
-    npm link testcheck
+    printf "\n\e[43;30m Test Integration: ${integration} \e[0m\n\n"
+    pushd ${integration} > /dev/null
+    npm link testcheck > /dev/null
     npm install
     npm test
-
-    popd
+    popd > /dev/null
   fi
 done
 
