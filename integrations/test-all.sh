a2389eb04d7ad216161082aadc6fecd0810e6eec
Lee Byron <lee@leebyron.com>
2016-12-20 14:47:49 -0800

    Simplify test script

--- integrations/test-all.sh
+++ integrations/test-all.sh
@@ -7,14 +7,12 @@ npm link > /dev/null
 
 # Loop through integrations, testing each
 for integration in ./*/; do
-  # if [ -d "${integration}" ] ; then
-    printf "\n\e[43;30m Test Integration: ${integration} \e[0m\n\n"
-    pushd ${integration} > /dev/null
-    npm link testcheck > /dev/null
-    npm install
-    npm test
-    popd > /dev/null
-  # fi
+  printf "\e[43;30m Test Integration: ${integration} \e[0m\n\n"
+  pushd ${integration} > /dev/null
+  npm link testcheck > /dev/null
+  npm install
+  npm test
+  popd > /dev/null
 done
 
 popd > /dev/null
