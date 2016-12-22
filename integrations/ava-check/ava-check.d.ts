d0750511254316f49b901a44a033906fd1a39a22
Lee Byron <lee@leebyron.com>
2016-12-22 12:43:03 -0800

    Fix test context for tsc in ava-check

--- integrations/ava-check/ava-check.d.ts
+++ integrations/ava-check/ava-check.d.ts
@@ -2,66 +2,65 @@
 
 export { gen } from 'testcheck'
 import { Generator, CheckOptions } from 'testcheck'
-
-type Test<T> = (t: T) => void;
+import { ContextualTest, ContextualTestContext } from 'ava'
 
 export function check<T,A>(
   genA: Generator<A>,
-  f: (t: T, a: A) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A) => boolean | void
+): ContextualTest;
 export function check<T,A,B>(
   genA: Generator<A>,
   genB: Generator<B>,
-  f: (t: T, a: A, b: B) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C>(
   genA: Generator<A>,
   genB: Generator<B>,
   genC: Generator<C>,
-  f: (t: T, a: A, b: B, c: C) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C,D>(
   genA: Generator<A>,
   genB: Generator<B>,
   genC: Generator<C>,
   genD: Generator<D>,
-  f: (t: T, a: A, b: B, c: C, d: D) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C, d: D) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C,D,E>(
   genA: Generator<A>,
   genB: Generator<B>,
   genC: Generator<C>,
   genD: Generator<D>,
   genE: Generator<E>,
-  f: (t: T, a: A, b: B, c: C, d: D, e: E) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C, d: D, e: E) => boolean | void
+): ContextualTest;
 
 export function check<T,A>(
   options: CheckOptions,
   genA: Generator<A>,
-  f: (t: T, a: A) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A) => boolean | void
+): ContextualTest;
 export function check<T,A,B>(
   options: CheckOptions,
   genA: Generator<A>,
   genB: Generator<B>,
-  f: (t: T, a: A, b: B) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C>(
   options: CheckOptions,
   genA: Generator<A>,
   genB: Generator<B>,
   genC: Generator<C>,
-  f: (t: T, a: A, b: B, c: C) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C,D>(
   options: CheckOptions,
   genA: Generator<A>,
   genB: Generator<B>,
   genC: Generator<C>,
   genD: Generator<D>,
-  f: (t: T, a: A, b: B, c: C, d: D) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C, d: D) => boolean | void
+): ContextualTest;
 export function check<T,A,B,C,D,E>(
   options: CheckOptions,
   genA: Generator<A>,
@@ -69,5 +68,5 @@ export function check<T,A,B,C,D,E>(
   genC: Generator<C>,
   genD: Generator<D>,
   genE: Generator<E>,
-  f: (t: T, a: A, b: B, c: C, d: D, e: E) => boolean | void
-): Test<T>;
+  f: (t: ContextualTestContext, a: A, b: B, c: C, d: D, e: E) => boolean | void
+): ContextualTest;
