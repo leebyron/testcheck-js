9646e57daa6cb58e02d2d62efbdb6e97a25fb369
Gabe Scholz <scholz.gabe@gmail.com>
2017-08-21 11:43:45 -0700

    Add TypeScript defs for jasmine-check

---
+++ integrations/jasmine-check/type-definitions/jasmine-check.d.ts
@@ -0,0 +1,71 @@
+import { Generator } from 'testcheck';
+
+declare global {
+  type Check<Key extends string> = {
+    [checker in Key]: {
+      (description: string): void;
+      (description: string, fn: () => void): void;
+      <A>(description: string, genA: Generator<A>, fn: (a: A) => void): void;
+      <A, B>(description: string, genA: Generator<A>, genB: Generator<B>, fn: (a: A, b: B) => void): void;
+      <A, B, C>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        fn: (a: A, b: B, c: C) => void,
+      ): void;
+      <A, B, C, D>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        fn: (a: A, b: B, c: C, d: D) => void,
+      ): void;
+      <A, B, C, D, E>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        fn: (a: A, b: B, c: C, d: D, e: E) => void,
+      ): void;
+      <A, B, C, D, E, F>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F) => void,
+      ): void;
+      <A, B, C, D, E, F, G>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        genG: Generator<G>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => void,
+      ): void;
+      <A, B, C, D, E, F, G, H>(
+        description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        genG: Generator<G>,
+        genH: Generator<H>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => void,
+      ): void;
+    }
+  };
+
+  export var check: Check<'it' | 'xit' | 'iit' | 'fit'> & { it: Check<'only' | 'skip'> };
+}
