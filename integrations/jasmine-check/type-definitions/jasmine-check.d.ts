1148ecfc2246dae458114dd70f71177899f3dd4a
Gabe Scholz <scholz.gabe@gmail.com>
2017-08-21 12:27:37 -0700

    Add options argument

--- integrations/jasmine-check/type-definitions/jasmine-check.d.ts
+++ integrations/jasmine-check/type-definitions/jasmine-check.d.ts
@@ -1,12 +1,21 @@
-import { Generator } from 'testcheck';
+import { Generator, CheckOptions } from 'testcheck';
 
 declare global {
   type Check<Key extends string> = {
     [checker in Key]: {
-      (description: string): void;
+      (description: string, opts?: CheckOptions): void;
       (description: string, fn: () => void): void;
+      (description: string, opts: CheckOptions, fn: () => void): void;
       <A>(description: string, genA: Generator<A>, fn: (a: A) => void): void;
+      <A>(description: string, opts: CheckOptions, genA: Generator<A>, fn: (a: A) => void): void;
       <A, B>(description: string, genA: Generator<A>, genB: Generator<B>, fn: (a: A, b: B) => void): void;
+      <A, B>(
+        description: string,
+        opts: CheckOptions,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        fn: (a: A, b: B) => void,
+      ): void;
       <A, B, C>(
         description: string,
         genA: Generator<A>,
@@ -14,8 +23,25 @@ declare global {
         genC: Generator<C>,
         fn: (a: A, b: B, c: C) => void,
       ): void;
+      <A, B, C>(
+        description: string,
+        opts: CheckOptions,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        fn: (a: A, b: B, c: C) => void,
+      ): void;
       <A, B, C, D>(
         description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        fn: (a: A, b: B, c: C, d: D) => void,
+      ): void;
+      <A, B, C, D>(
+        description: string,
+        opts: CheckOptions,
         genA: Generator<A>,
         genB: Generator<B>,
         genC: Generator<C>,
@@ -31,8 +57,29 @@ declare global {
         genE: Generator<E>,
         fn: (a: A, b: B, c: C, d: D, e: E) => void,
       ): void;
+      <A, B, C, D, E>(
+        description: string,
+        opts: CheckOptions,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        fn: (a: A, b: B, c: C, d: D, e: E) => void,
+      ): void;
       <A, B, C, D, E, F>(
         description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F) => void,
+      ): void;
+      <A, B, C, D, E, F>(
+        description: string,
+        opts: CheckOptions,
         genA: Generator<A>,
         genB: Generator<B>,
         genC: Generator<C>,
@@ -52,8 +99,33 @@ declare global {
         genG: Generator<G>,
         fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => void,
       ): void;
+      <A, B, C, D, E, F, G>(
+        description: string,
+        opts: CheckOptions,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        genG: Generator<G>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => void,
+      ): void;
       <A, B, C, D, E, F, G, H>(
         description: string,
+        genA: Generator<A>,
+        genB: Generator<B>,
+        genC: Generator<C>,
+        genD: Generator<D>,
+        genE: Generator<E>,
+        genF: Generator<F>,
+        genG: Generator<G>,
+        genH: Generator<H>,
+        fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => void,
+      ): void;
+      <A, B, C, D, E, F, G, H>(
+        description: string,
+        opts: CheckOptions,
         genA: Generator<A>,
         genB: Generator<B>,
         genC: Generator<C>,
