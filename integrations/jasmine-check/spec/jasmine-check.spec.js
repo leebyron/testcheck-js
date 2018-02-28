551414abe3185e167e8501c5c0a4d2869284e1a5
David Normington <david.normington@arcadiagroup.co.uk>
2018-02-28 21:16:39 +0000

    Fixing indentation

--- integrations/jasmine-check/spec/jasmine-check.spec.js
+++ integrations/jasmine-check/spec/jasmine-check.spec.js
@@ -1,61 +1,61 @@
 const { install } = require('../jasmine-check')
 
 describe('jasmine-check', () => {
-	let registeredSpec
-	let specResult
-	let spec
+  let registeredSpec
+  let specResult
+  let spec
 
-	beforeEach(() => {
-		spec = {
-			addMatcherResult: (result) => {
-				specResult = result
-			},
-			result: {}
-		}
-	})
+  beforeEach(() => {
+    spec = {
+      addMatcherResult: (result) => {
+        specResult = result
+      },
+      result: {}
+    }
+  })
 
-	 
-	let env = {
-		jasmine: {
-			getEnv: () => ({ version: () => 2 })
-		},
-		expect: (expected) => ({
-			toBe: (actual) => spec.addMatcherResult({ 
-				expected,
-				actual,
-				passed: () => expected === actual
-			})
-		}),
-		it: (desc, run) => {
-			spec.description = desc
-			registeredSpec = { desc, run }
-			return spec
-		} 
-	}
-	const runSpec = () => {
-		spec.result.fullName = registeredSpec.desc
-		spec.result.description = registeredSpec.desc
-		registeredSpec.run()
-		return spec
-	}
+   
+  let env = {
+    jasmine: {
+      getEnv: () => ({ version: () => 2 })
+    },
+    expect: (expected) => ({
+      toBe: (actual) => spec.addMatcherResult({ 
+        expected,
+        actual,
+        passed: () => expected === actual
+      })
+    }),
+    it: (desc, run) => {
+      spec.description = desc
+      registeredSpec = { desc, run }
+      return spec
+    } 
+  }
+  const runSpec = () => {
+    spec.result.fullName = registeredSpec.desc
+    spec.result.description = registeredSpec.desc
+    registeredSpec.run()
+    return spec
+  }
 
-	it('runs a passing test', () => {
+  it('runs a passing test', () => {
     install(env)
 
     env.check.it('a passing test', env.gen.int, (x) => {
-    	env.expect(x).toBe(x)
+      env.expect(x).toBe(x)
     })
 
     let result = runSpec()
     
     expect(specResult.passed()).toBe(true)
-	})
+  })
 
-	it('runs a failing test', () => {
-		install(env)
+  it('runs a failing test', () => {
+    install(env)
 
     env.check.it('a failing test', env.gen.int, (x) => {
-    	env.expect(x).toBe('foo')
+      env.expect(x).toBe('foo')
     })
 
     let spec = runSpec()
@@ -65,5 +65,5 @@ describe('jasmine-check', () => {
     expect(spec.description).toMatch(resultRe)
     expect(spec.result.description).toMatch(resultRe)
     expect(spec.result.fullName).toMatch(resultRe)
-	})
+  })
 })
